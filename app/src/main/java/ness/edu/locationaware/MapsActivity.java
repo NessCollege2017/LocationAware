2b2110737c2753c1c126b2188b839d6fa0bcb9e5
NessCollege <nesscollege2017@gmail.com>
2017-07-04 20:03:59 +0300

    Last Known Location Fragment

--- app/src/main/java/ness/edu/locationaware/MapsActivity.java
+++ app/src/main/java/ness/edu/locationaware/MapsActivity.java
@@ -23,20 +23,14 @@ public class MapsActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        //Find an existing fragment in the XML (Static Approach)
-        /*
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        */
 
         //in the dynamic approach -> instantiate the fragment
         MyMapFragment mapFragment = new MyMapFragment();
 
         getSupportFragmentManager().beginTransaction().
-                replace(R.id.frame1, mapFragment).
+                replace(R.id.frame1, mapFragment, "1").
+                replace(R.id.frame2, new LocationFragment(), "2").
                 commit();
-
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -78,16 +72,6 @@ public class MapsActivity extends AppCompatActivity{
     //1) declare a listener: AuthStateListener
     //2) register the listener in onResume
     //3) remove the listener in onPause
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
 
 
 }
